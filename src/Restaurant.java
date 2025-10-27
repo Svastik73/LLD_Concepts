5561f60179e25e388f9c02fcefba61efcf68dcfb
Svastik Kanwar <kanwar7svastik@gmail.com>
2025-10-27 11:14:39 +0530

    added files

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,4 +1,37 @@
-public class Restaurant {
+interface RestaurantEmployee {
     // Interface segmented principle example.
+    void washDishes();
+    void serveCustomers();
+    void cookFood();
+}
+class waiter implements RestaurantEmployee{
+    public void washDishes(){
+        // no use!
+    }
+    public void serveCustomers(){
+        // only must implement
+    }
+    public void cookFood(){
+        // no use! here
+    }
+}
+/// ////////////////////////////
+// instead make other interface
+interface WaiterInterface{
+    void serveCustomers();
+    void takeOrders();
+}
+interface ChefInterface{
+    void cookFood();
+    void decideMenu();
+}
+class Waiter1 implements WaiterInterface{
+    @Override
+    public void serveCustomers() {
 
+    }
+    @Override
+    public void takeOrders() {
+
+    }
 }
